5ba783f59e4200880a3630fc35bd605684f3c15e
Farseen CK <itsfarseen@gmail.com>
2020-07-14 01:02:25 +0530

    Create a tun interface, print receiving packets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let iface = tun_tap::Iface::without_packet_info("", tun_tap::Mode::Tun)
+        .expect("Failed to initialize TUN interface");
+    let mut buf = [0u8; 2000];
+    loop {
+        let read = iface.recv(&mut buf).expect("Failed to read");
+        let (iph, ippld) =
+            if let Ok((iph, pld)) = etherparse::Ipv4Header::read_from_slice(&buf[0..read]) {
+                (iph, pld)
+            } else {
+                continue;
+            };
+        
+        let (tcph, tcppld) =
+            if let Ok((tcph, tcppld)) = etherparse::TcpHeader::read_from_slice(ippld) {
+                (tcph, tcppld)
+            } else {
+                continue;
+            };
+
+        dbg!((tcph, tcppld));
+    }
 }
