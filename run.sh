3cc0104b1f753aa195da104b59ac381ce2f6c928
Farseen CK <itsfarseen@gmail.com>
2020-07-14 18:01:31 +0530

    Basic connection establishment

--- run.sh
+++ run.sh
@@ -8,5 +8,9 @@ if cargo build; then
     sudo ip addr add 10.0.0.1 dev tun0
     sudo ip link set tun0 up
     sudo ip route add 10.0.0.0/24 dev tun0
+
+    if [ -n "$TMUX" ]; then
+        tmux respawn-pane -k -t 0.1 "tshark -i tun0 -f tcp; bash"
+    fi;
     wait $pid
 fi
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,85 @@
-fn main() {
-    let iface = tun_tap::Iface::without_packet_info("", tun_tap::Mode::Tun)
-        .expect("Failed to initialize TUN interface");
-    let mut buf = [0u8; 2000];
-    loop {
-        let read = iface.recv(&mut buf).expect("Failed to read");
-        let (iph, ippld) =
-            if let Ok((iph, pld)) = etherparse::Ipv4Header::read_from_slice(&buf[0..read]) {
-                (iph, pld)
-            } else {
-                continue;
-            };
-        
-        let (tcph, tcppld) =
-            if let Ok((tcph, tcppld)) = etherparse::TcpHeader::read_from_slice(ippld) {
-                (tcph, tcppld)
-            } else {
-                continue;
-            };
+use etherparse::{IpTrafficClass, Ipv4Header, TcpHeader};
 
-        dbg!((tcph, tcppld));
+fn main() {
+    let mut tcp = {
+        let iface = tun_tap::Iface::without_packet_info("", tun_tap::Mode::Tun)
+            .expect("Failed to initialize TUN interface");
+        TCP::with_iface(iface)
+    };
+
+    tcp.run();
+}
+
+struct TCP {
+    iface: tun_tap::Iface,
+    buf: [u8; 2000],
+}
+
+impl TCP {
+    pub fn with_iface(iface: tun_tap::Iface) -> Self {
+        Self {
+            iface,
+            buf: [0u8; 2000],
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let read = self.iface.recv(&mut self.buf).expect("Failed to read");
+            let (iph, ippld) =
+                if let Ok((iph, pld)) = Ipv4Header::read_from_slice(&self.buf[0..read]) {
+                    (iph, pld)
+                } else {
+                    // eprintln!("Dropping packet: not IPv4");
+                    continue;
+                };
+
+            if iph.protocol != 6 {
+                // eprintln!("Dropping packet: not TCP");
+                continue;
+            }
+
+            let (tcph, tcppld) = TcpHeader::read_from_slice(ippld).expect("Failed to parse TCP");
+
+            eprintln!("Received segment: ");
+            dbg!(&iph);
+            dbg!((&tcph, &tcppld));
+
+            // Waiting for connection:
+            // Incoming packet must be SYN.
+            if !tcph.syn {
+                eprintln!("Dropping seg: state LISTEN, seg not SYN");
+                continue;
+            }
+
+            // ACK the SYN, send our SYN
+            let mut tcph1 = TcpHeader::new(
+                tcph.destination_port, // Incoming destination is our source
+                tcph.source_port,      // Incoming source is our destt
+                123445,                     // Sequence Number. FIXME
+                1000,                  // Window size. FIXME
+            );
+
+            tcph1.syn = true;
+            tcph1.ack = true;
+            tcph1.acknowledgment_number = tcph.sequence_number + 1;
+
+            let iph1 = Ipv4Header::new(
+                tcph1.header_len(),  // Payload length
+                64,                  // Time to live. (Default taken from wikipedia)
+                IpTrafficClass::Tcp, // Protocol
+                iph.destination,
+                iph.source,
+            );
+
+            let mut buf = &mut self.buf[..];
+
+            iph1.write(&mut buf).unwrap();
+
+            tcph1.checksum = tcph1.calc_checksum_ipv4(&iph1, &[]).unwrap();
+            tcph1.write(&mut buf).unwrap();
+
+            self.iface.send(&self.buf).unwrap();
+        }
     }
 }
